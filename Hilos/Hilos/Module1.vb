bbc5c693665850c71f5e7e9b00734a111d149cfd
fsastoque <fsastoque@gmail.com>
2018-11-29 10:11:16 -0500

    adicion clase tareas

--- Hilos/Hilos/Module1.vb
+++ Hilos/Hilos/Module1.vb
@@ -1,7 +1,35 @@
-﻿Module Module1
+﻿Imports System.Threading
+Module Module1
 
     Sub Main()
+        '**Con Arrays
 
+        Dim listTareas As New ArrayList
+        listTareas.Add("Hilo1")
+        listTareas.Add("Hilo2")
+        listTareas.Add("Hilo3")
+        Dim listSecond As New ArrayList
+        listSecond.Add(8)
+        listSecond.Add(16)
+        listSecond.Add(24)
+        For i = 0 To listSecond.Count - 1
+            Dim tareas As String = Convert.ToString(listTareas(i))
+            Dim second As Integer = Convert.ToString(listSecond(i))
+            Dim obj1 As New Tareas(tareas, second)
+            Dim hilo1 As New Thread(AddressOf obj1.Tarea1)
+            hilo1.Start()
+        Next
+
+        '****con variables
+
+        'Dim obj1 As New Tareas("hilo1", 8)
+        'Dim obj2 As New Tareas("hilo2", 16)
+        ''AddressOf asigna la funcion a ejecutar
+        'Dim hilo1 As New Thread(AddressOf obj1.Tarea1)
+        'Dim hilo2 As New Thread(AddressOf obj2.Tarea2)
+        'hilo1.Start()
+        'hilo2.Start()
+        'Console.ReadKey()
     End Sub
 
 End Module
---
+++ Hilos/Hilos/Tareas.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Threading
+Public Class Tareas
+    Private name As String
+    Private second As Integer
+    Sub New(name As String, second As Integer)
+        Me.name = name
+        Me.second = second
+    End Sub
+    Public Sub Tarea1()
+        Console.WriteLine(name & " Esta tarea solo tendra " & second & " second . ")
+        'Sleep suspende el subproceso actual durante un tiempo
+        Thread.Sleep(second * 1000)
+        Console.WriteLine("Tarea terminada: " & name)
+    End Sub
+    Public Sub Tarea2()
+        Console.WriteLine(name & " Esta tarea solo tendra " & second & " second . ")
+        'Sleep suspende el subproceso actual durante un tiempo
+        Thread.Sleep(second * 1000)
+        Console.WriteLine("Tarea terminada: " & name)
+    End Sub
+
+End Class
